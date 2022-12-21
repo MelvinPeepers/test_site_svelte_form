138e0532e8201d5b8e75215bb65ec38b854b6c95
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2022-12-21 14:02:58 -0500

    form setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+	export const prerender = true;
+</script>
+
+
+<form name="netlify-form-example" method="POST" netlify-honeypot="bot-field" data-netlify="true">
+	<input type="hidden" name="form-name" value="netlify-form-example" />
+	<input name="name" required placeholder="Name" type="name" />
+	<br />
+	<input name="email" required placeholder="Email" type="email" />
+	<br />
+	<input name="message" required placeholder="Message" type="text" />
+	<br />
+	<input type="submit" value="Submit" />
+</form>
